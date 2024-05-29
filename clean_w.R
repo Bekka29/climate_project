ae1f50963936b84164a787292326513a631d722a
Bekka29 <rebekahgambo@gmail.com>
2024-05-29 08:53:01 +0200

    cleaned code

--- clean_w.R
+++ clean_w.R
@@ -45,41 +45,40 @@ View(tdata)
 
 #Summarizing and Grouping data---------------------------------------------------------------- 
 #determine mean of cases per state and year so as to fill in missing values in "cases" column
- mean_cases <- tdata %>%
-    group_by(state, year) %>%
-    summarise(mean_cases = mean(cases, na.rm = TRUE)) %>%
-    print()
- 
- 
-#Merge mean_cases with tdata based on state and year to fill missing values in "cases" column
-  tdata_fill <-tdata %>%
-    left_join(mean_cases, by =c("state", "year")) %>%
-    mutate(cases = ifelse(is.na(cases), mean_cases, cases)) %>%
-    select(-mean_cases)
-#Print the filled data frame
-  print(tdata_fill)
-  
-  
-# Round the values in the "cases" column to 1 decimal place
-  tdata_fill <-tdata_fill %>%
-    mutate(cases = round(cases,1))
-  print(tdata_fill)
-   
-View(tdata_fill)
+#  mean_cases <- tdata %>%
+#     group_by(state, year) %>%
+#     summarise(mean_cases = mean(cases, na.rm = TRUE)) %>%
+#     print()
+#  
+# #Merge mean_cases with tdata based on state and year to fill missing values in "cases" column
+#   tdata_fill <-tdata %>%
+#     left_join(mean_cases, by =c("state", "year")) %>%
+#     mutate(cases = ifelse(is.na(cases), mean_cases, cases)) %>%
+#     select(-mean_cases)
+# #Print the filled data frame
+#   print(tdata_fill)
+#   
+# # Round the values in the "cases" column to 1 decimal place
+#   tdata_fill <-tdata_fill %>%
+#     mutate(cases = round(cases,1))
+#   print(tdata_fill)
+#    
+# View(tdata_fill)
 
 #determine mean and standard deviation of climate variables  
-mean_climate <- tdata_fill  %>%
+mean_data <- tdata  %>%
   group_by(state,year) %>%
   summarise( meanw_tempmin = mean(w_tempmin),
              meanw_temp = mean(w_temp),
              meanw_tempmax = mean(w_tempmax),
              meanw_precip = mean(w_precip),
              meanw_precov = mean(w_precov),
-             meanw_humid = mean(w_humid))
-print(mean_climate)
+             meanw_humid = mean(w_humid),
+             mean_cases = mean(cases))
+print(mean_data)
 
 #Calculate standard deviations for all variables
-standard_deviations <- tdata_fill %>%
+standard_deviations <- tdata %>%
   group_by(state, year) %>%
   summarise(sd_cases = sd(cases),
             sd_w_tempmin = sd(w_tempmin),
@@ -91,25 +90,10 @@ standard_deviations <- tdata_fill %>%
 
 print(standard_deviations)
 
-#create table for mean and standard deviations
 
- #Merge mean cases and mean climate
-  merged_table <- left_join(mean_cases, mean_climate, by = c("state", "year"))
-  print(merged_table)
-
-#Calculate standard deviations for climate variables
-standard_deviations <- tdata_fill %>%
-  group_by(state, year) %>%
-  summarise(sd_cases = sd(cases),
-            sd_w_tempmin = sd(w_tempmin),
-            sd_w_temp = sd(w_temp),
-            sd_w_tempmax = sd(w_tempmax),
-            sd_w_humid = sd(w_humid),
-            sd_w_precip = sd(w_precip),
-            sd_w_precov = sd(w_precov))
 
 # Merge standard deviations with the previously merged table
-  merged_table <- left_join(merged_table, standard_deviations, by = c("state", "year"))
+  merged_table <- left_join(mean_data, standard_deviations, by = c("state", "year"))
   print(merged_table)
 
 View(merged_table)
@@ -119,17 +103,13 @@ View(merged_table)
             #Determination of data distribution 
 
 #cases
-histogram <- tdata_fill %>%
+histogram <- tdata %>%
   ggplot(aes(x = cases)) + geom_histogram(bins = 10) +  facet_grid(state ~ year) +   
   labs(x = "Cases", y = "Frequency", title = "Cases by State and Year")
 print(histogram)
 
-histogram <- tdata_fill %>%
-  ggplot(aes(x = w_temp)) + geom_histogram(bins = 10) +  facet_grid(state ~ year) +   
-  labs(x = "Temperature", y = "Frequency", title = "Temperature by State and Year")
-print(histogram)
 
-histogram <- tdata_fill %>%
+histogram <- tdata %>%
   ggplot(aes(x = w_precip)) + geom_histogram(bins = 10) +  facet_grid(state ~ year) +   
   labs(x = "precipitation", y = "Frequency", title = "Precipitation by State and Year")
 print(histogram)
@@ -138,7 +118,7 @@ print(histogram)
               #Visualization of epidemiological data 
 
 #bar chart of total number of cases per year by epiweek
-  ggplot(data = tdata_fill, aes(x = epiweek, y = cases)) +
+  ggplot(data = tdata, aes(x = epiweek, y = cases)) +
     geom_bar(stat = "identity") +
     facet_wrap(~ state) +
     labs(x = "Epiweek", y = "Cases", title = "Cases by Epiweek")
@@ -150,21 +130,21 @@ print(histogram)
 #     labs(x = "Epiweek", y = "cases", title = "Cases by Epiweek per Year") 
 
 #side by side bar chart: Number of cases per epiweek across states
-  ggplot(data = tdata_fill, aes(x = epiweek, y = cases, fill = state))+ geom_bar (stat ="identity", position = "dodge") + 
+  ggplot(data = tdata, aes(x = epiweek, y = cases, fill = state))+ geom_bar (stat ="identity", position = "dodge") + 
     facet_wrap(~ year) + labs(title = "Weekly Case count across States") 
   
 #line graph of cases by epiweek across years
-  ggplot(data = tdata_fill, aes(x = epiweek, y = cases, group = interaction(state, year), color = year)) + 
+  ggplot(data = tdata, aes(x = epiweek, y = cases, group = interaction(state, year), color = year)) + 
     geom_line() + facet_wrap(~ state) + labs(x = "Epiweek", y = "cases", title = "Cases by Epiweek") +
     theme_bw() +theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
   
+#line graph of cases by epiweek across years  
+  epiweek_levels <- as.numeric(levels(tdata$epiweek))
   
-  epiweek_levels <- as.numeric(levels(tdata_fill$epiweek))
-  
-  # Determine breaks (e.g., every 4th epiweek)
+  #Determine breaks (e.g., every 4th epiweek)
   breaks <- epiweek_levels[seq(1, length(epiweek_levels), by = 3)]
   
-  ggplot(data = tdata_fill, aes(x = epiweek, y = cases, group = interaction(state, year), color = year)) + 
+  ggplot(data = tdata, aes(x = epiweek, y = cases, group = interaction(state, year), color = year)) + 
     geom_line() + 
     facet_wrap(~ state) + 
     labs(x = "Epiweek", y = "cases", title = "Case count by Epiweek across years") +
@@ -173,12 +153,19 @@ print(histogram)
     scale_x_discrete(breaks = breaks)
   
   
+#side by side barchart of cases per epiweek
   
+  #Determine breaks (e.g., every 4th epiweek)
+  breaks <- epiweek_levels[seq(1, length(epiweek_levels), by = 3)]
   
-  
-  
-  
-  
+  # barchart with vertical month labels
+  ggplot(data = tdata, aes(x = epiweek, y = cases, fill = state)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    facet_wrap(~ year) +
+    labs(title = "Case Count by Epiweek") +
+    theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust = 1))+theme_bw() +
+    scale_x_discrete(breaks = breaks) +
+    scale_y_continuous(breaks = seq(0, max(tdata$cases), by =5))
   
   
   
@@ -186,15 +173,15 @@ print(histogram)
           #Visualization of climate data 
   
 #line graph of temperature by epiweek across years
-  ggplot(data = tdata_fill, aes(x = epiweek, y = w_temp, group = interaction(state, year), color = year)) + 
-    geom_line() + facet_wrap(~ state) + labs(x = "Epiweek", y = "temperature", title = "Temperature by Epiweek") 
+  ggplot(data = tdata, aes(x = epiweek, y = w_temp, group = interaction(state, year), color = year)) + 
+    geom_line() + facet_wrap(~ state) + labs(x = "Epiweek", y = "temperature", title = "Temperature by Epiweek")
   
 #line graph of precipitation by epiweek across years
-  ggplot(data = tdata_fill, aes(x = epiweek, y = w_precip, group = interaction(state, year), color = year)) + 
+  ggplot(data = tdata, aes(x = epiweek, y = w_precip, group = interaction(state, year), color = year)) + 
     geom_line() + facet_wrap(~ state) + labs(x = "Epiweek", y = "precipitation", title = "Precipitation by Epiweek") 
   
 #line graph of humidity by epiweek across years
-  ggplot(data = tdata_fill, aes(x = epiweek, y = w_humid, group = interaction(state, year), color = year)) + 
+  ggplot(data = tdata, aes(x = epiweek, y = w_humid, group = interaction(state, year), color = year)) + 
     geom_line() + facet_wrap(~ state) + labs(x = "Epiweek", y = "humidity", title = "Humidity by Epiweek")
   
 #Assessing the relationship between variables----------------------------------------------------------------------------------------------
@@ -202,64 +189,75 @@ print(histogram)
 #Scatter plots (relationship between continuous variables)
 
   #cases versus precipitation  
-  ggplot(data = tdata_fill, aes(x = w_precip, y = cases, color = year)) + geom_point() +
+  ggplot(data = tdata, aes(x = w_precip, y = cases, color = year)) + geom_point(size=2) +
     facet_wrap(~ state) +
-    labs(x = "precipitation", y = "cases", title = "Cases against Precipitation by State")
+    labs(x = "precipitation", y = "cases", title = "Cases against Precipitation by State") +
+    geom_smooth(method =lm, se = F) + theme_bw()
+  
   
   #cases versus precipitation cover  
-  ggplot(data = tdata_fill, aes(x = w_precov, y = cases, color = year)) + geom_point() +
+  ggplot(data = tdata, aes(x = w_precov, y = cases, color = year)) + geom_point(size=2) +
     facet_wrap(~ state) +
-    labs(x = "precipitation cover", y = "cases", title = "Cases against Precipitation cover by State")
-
+    labs(x = "precipitation cover", y = "cases", title = "Cases against Precipitation cover by State") +
+    geom_smooth(method =lm, se = F) + theme_bw()
+  
   #cases versus humidity  
-  ggplot(data = tdata_fill, aes(x = w_humid, y = cases, color = year)) + geom_point() +
+  ggplot(data = tdata, aes(x = w_humid, y = cases, color = year)) + geom_point(size=2) +
     facet_wrap(~ state) +
-    labs(x = "humidity", y = "cases", title = "Cases against humidity by State")
-
+    labs(x = "humidity", y = "cases", title = "Cases against humidity by State")+
+    geom_smooth(method =lm, se = F) + theme_bw()
+  
   #cases versus temperature  
-  ggplot(data = tdata_fill, aes(x = w_temp, y = cases, color = year)) + geom_point() +
+  ggplot(data = tdata, aes(x = w_temp, y = cases, color = year)) + geom_point(size=2) +
     facet_wrap(~ state) +
-    labs(x = "temperature", y = "cases", title = "Cases against Temperature by State")
+    labs(x = "temperature", y = "cases", title = "Cases against Temperature by State")+
+    geom_smooth(method =lm, se = F) + theme_bw()
   
   #cases versus minimum temperature
-  ggplot(data = tdata_fill, aes(x = w_tempmin, y = cases, color = state)) + geom_point() +
+  ggplot(data = tdata, aes(x = w_tempmin, y = cases, color = state)) + geom_point(size=2) +
     facet_wrap(~ year) +
-    labs(x = "minimum temperature", y = "cases", title = "Cases against Minimum Temperature by year")
-  
-  #cases versus maximum temperature
-  ggplot(data = tdata_fill, aes(x = w_tempmax, y = cases, color = state)) + geom_point() +
+    labs(x = "minimum temperature", y = "cases", title = "Cases against Minimum Temperature by year")+
+    geom_smooth(method =lm, se = F) + theme_bw()
+ 
+   #cases versus maximum temperature
+  ggplot(data = tdata, aes(x = w_tempmax, y = cases, color = state)) + geom_point(size=2) +
     facet_wrap(~ year) +
-    labs(x = "maximum temperature", y = "cases", title = "Cases against Maximum Temperature by year")
-  
+    labs(x = "maximum temperature", y = "cases", title = "Cases against Maximum Temperature by year")+
+    geom_smooth(method =lm, se = F) + theme_bw()
 
 #Correlation analysis---------------------------------------------------------------------------------------------
   
 #Spearman's correlation for direction and magnitude of relationship (data does not have a normal distribution
-  
-#Creating a function that will return correlations for each group
-  calculate_group_correlations <- function(tdata_fill) {
-    correlations <- tdata_fill %>%
+
+#correlation table grouped by state and year
+ #Creating a function to return correlations for each group
+  calculate_group_correlations <- function(tdata) {
+    correlations <- tdata %>%
       select(cases, w_tempmin, w_temp, w_tempmax, w_humid, w_precip, w_precov) %>%
       correlate(method = "spearman")
     return(correlations)
   }
-#applying the function to create a grouped correlation table
-  correlation_tab <- tdata_fill %>%
+#applying the function
+  correlation_tab <- tdata %>%
     group_by(state, year) %>%
     do(calculate_group_correlations(.))
    print(correlation_tab)
   
 View (correlation_tab)
   
-#Correlation table for ungrouped data  
+#Correlation table for data grouped by state  
   
-  correlation_table <- tdata_fill %>% 
-    ungroup()
-  
-  correlation_results <- correlation_table %>% 
-    select(cases, w_tempmin, w_temp, w_tempmax, w_humid, w_precip, w_precov) %>%   
-    correlate(method = "spearman")    
-  print(correlation_results)
+calculate_group_correlations <- function(tdata) {
+  correlations <- tdata %>%
+    select(cases, w_tempmin, w_temp, w_tempmax, w_humid, w_precip, w_precov) %>%
+    correlate(method = "spearman")
+  return(correlations)
+}
 
-View(correlation_results)
+correlation_tab <- tdata %>%
+  group_by(state) %>%
+  do(calculate_group_correlations(.))
+print(correlation_tab)
+
+View (correlation_tab)
    
